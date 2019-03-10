fcfc93302e95db9d6d496e27130ee1ed61fa486e
muhammet-kandemir-95 <k8a1n6d3e3m7i11r4@hotmail.com>
2019-03-10 11:24:33 +0300

    Added main js

---
+++ components/dmuka2/scripts/main.js
@@ -0,0 +1,19 @@
+/**
+ * Name				:	dmuka2
+ * Author			:	Muhammet Kandemir (github.com/muhammet-kandemir-95)
+ * Version			:	2.0.0.0
+ * Description		:	This is only a library which has some components. 
+ * 						It can't anything on own.
+ * 						So you should use with components.
+ * 						Components list is in README.md.
+ */
+
+//#region Creating NameSpace
+if(window['dmuka2'] === undefined){
+	window['dmuka2'] = {};
+}
+
+if(window['dmuka2']['Html'] === undefined){
+	window['dmuka2']['Html'] = {};
+}
+//#endregion
